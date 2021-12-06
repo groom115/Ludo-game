8ded542f52287dc51b5d71ca652721ce815776d5
groom115 <79688511+groom115@users.noreply.github.com>
2021-12-06 12:45:35 +0530

    Create game.js

---
+++ game.js
@@ -0,0 +1,56 @@
+.container {
+        width: 70%;
+        margin: auto;
+        text-align: center;
+    }
+  
+    .dice {
+        text-align: center;
+        display: inline-block;
+        margin: 10px;
+    }
+  
+    body {
+        background-color: #ffccbcba;
+        margin: 0;
+    }
+  
+    h1 {
+        margin: 30px;
+        font-family: "Lobster", cursive;
+        text-shadow: 5px 0 #232931;
+        font-size: 4.5rem;
+        color: #36574c;
+        text-align: center;
+    }
+  
+    p {
+        font-size: 2rem;
+        color: #4ecca3;
+        font-family: "Indie Flower", cursive;
+    }
+  
+    img {
+        width: 100%;
+      height:150px;
+      width:150px;
+    }
+  
+    .bottom {
+        padding-top: 30px;
+    }
+  
+    .butn {
+        background: #2f4e44;
+        font-family: "Indie Flower", cursive;
+        border-radius: 7px;
+        color: #ffff;
+        font-size: 30px;
+        padding: 16px 25px 16px 25px;
+        text-decoration: none;
+    }
+  
+    .butn:hover {
+        background: #232931;
+        text-decoration: none;
+    }
